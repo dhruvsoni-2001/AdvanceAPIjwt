09e7942b447cf0dd371e3f867f182fffa2530dd8
PT Dhruv Soni <dhruvks@weblineapps.com>
2023-06-15 11:38:19 +0530

    Added Model and migration

---
+++ AdvanceAPIjwt/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AdvanceAPIjwt.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdvanceAPIjwt.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options) 
+        { 
+        }
+        public DbSet<RegisterUser> registerUsers { get; set; }
+    }
+   
+}
